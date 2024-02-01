e24d298d2b755efff06f29d2965adb99566997b8
djmagbuhos <dj.magbuhos@gmail.com>
2024-02-01 14:13:45 +0800

    added some controllers. add routes in web.php

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Signup</h1>
+    <form action="">
+        {{-- @csrf --}}
+        <label for="">First Name</label>
+        <input type="text" name="first_name" /><br />
+        <label for="">Last Name</label>
+        <input type="text" name="last_name" /><br />
+        <label for="">First Name</label>
+        <input type="text" name="first_name" /><br />
+        <label for="">First Name</label>
+        <input type="text" name="first_name" /><br />
+        <label for="">First Name</label>
+        <input type="text" name="first_name" /><br />
+
+
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/shop', [ProductController::class, 'show_all_products']);
+
+Route::get('/signup', [UserController::class, 'signup_show']);
+
 
 Route::get('/', function () {
     return view('welcome');
