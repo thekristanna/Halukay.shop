67168ddf7a132e49c659564fcf49855656bb7283
Pquiachon <johnpaulquiachon2904@gmail.com>
2024-02-02 11:19:43 +0800

    new modification in product table

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+
 
 class ProductController extends Controller
 {
-    //
+    public function show_all_products(Request $r)
+    {
+        $products = Product::query()
+            ->select('*');
+        if ($r->filled('category')) {
+            $products->where('category', '=', $r->input('category'));
+        }
+        if ($r->filled('nego')) {
+            $products->where('nego_status', '=', $r->input('nego'));
+        }
+        $products = $products
+            ->paginate(20);
+        $products->appends($r->except('page'));
+        return view('all_products', compact('products'));
+    }
 }
