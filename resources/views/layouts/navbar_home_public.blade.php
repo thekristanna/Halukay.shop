5835dd1d8e5b73cb23f5e31485dd1dd3c35fcf99
thekristanna <thekristanna@gmail.com>
2024-02-08 15:12:29 +0800

    home and navbar public fixes

--- resources/views/layouts/navbar_home_public.blade.php
+++ resources/views/layouts/navbar_home_public.blade.php
@@ -1,15 +1,21 @@
-<link rel="stylesheet" href="../css/home.css" />
+<link rel="stylesheet" href="../css/navbar_public.css" />
 <header>
-    <div class="brand">
-      <img src="../img/halukay-logo.png" alt="halukay-logo" id="logo">
-      <a href="#" id="brand">halukay.com</a>
-    </div>
-    <div class="navbar">
-      <a href="index.html" class="active" id="home">Home</a>
-      <a href="/about" id="about">About</a>
-      <a href="/shop" id="shop">Shop</a>
-      @include('layouts/search')      
-    </div>
-    <div><a href="/signup" id="register">Sign Up</a></div>
-    <div class="bx bx-menu" id="menu-icon"></div>
+  <div class="brand">
+    <img src="../img/halukay-logo.png" alt="halukay-logo" id="logo">
+    <a href="#" id="brand">halukay.shop</a>
+  </div>
+  <div class="navbar">
+    <a href="home.html" class="active" id="home">Home</a>
+            <a href="about.html" id="about">About</a>
+            <a href="shop.html" id="shop">Shop</a>
+            <a href="login.html" id="login-nav">Login</a>
+  </div>
+  <form class="search">
+      <input type="text" id="search-input" placeholder="Search here"/>
+      <button type="submit" id="search-button">
+        <i class="ri-search-line" id="search-icon"></i>
+      </button>
+    </form> 
+  <div class="login"><a href="#" id="login">Login</a></div>
+  <div class="bx bx-menu" id="menu-icon"></div>
 </header>
