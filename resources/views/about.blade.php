3404b326a86c26d834714ae8b6e2ebe7876d88ca
thekristanna <thekristanna@gmail.com>
2024-02-09 16:36:51 +0800

    fixed about blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,7 +19,7 @@
     <!-- Favicon -->
     <link rel="icon" href="/img/halukay-favicon.png" type="image/x-icon" />
     <!-- Page Title -->
-    <!-- <title>Halukay</title> -->
+    <title>About | Halukay</title>
   <body>
     <!-- header -->
     @if (Session::get('role') == 'seller')
@@ -29,8 +29,8 @@
     @else
         @include('layouts/navbar_home_public')
     @endif
-    <!-- about -->
-    <div class="container">
+     <!-- about -->
+     <div class="container">
         <h1>About Us</h1>
         <div class="about">
             <p>
@@ -73,10 +73,49 @@
             </div>
         </div>
     </div>
+<!-- footer -->
+<footer>
+  <div class="footer-brand">
+      <img
+          src="../img/halukay-favicon.png"
+          alt="halukay-logo"
+          id="footer-logo"
+      />
+      <a href="#" id="footer-brand">halukay.shop</a>
+  </div>
+  <div class="footer-proper">
+      <div class="subscription">
+          <p>
+              Stay in the loop and sign up for halukay<br />recommendations
+              from featured items
+          </p>
+          <input type="text" placeholder="Enter Email" id="email" /><i class="ri-arrow-right-line"></i>
+      </div>
+      <div class="wrapper">
+          <div class="company">
+              <p>Halukay</p>
+              <a href="home.html" id="home">Home</a>
+              <a href="about.html" id="about">About</a>
+              <a href="shop.html" id="shop">Shop</a>
+              <a href="contact.html" id="contact">Contact</a>
 
-      
-    </div>
-    <!-- footer -->
-    {{-- @include('layouts/footer') --}}
-  </body>
+          </div>
+          <div class="support">
+              <p>Support</p>
+              <a href="../pdfs/community_guidelines.pdf" download="community_guidelines.pdf" target="_blank" id="community">Community Guidelines</a>
+              <a href="../pdfs/terms_conditions.pdf" download="terms_conditions.pdf" target="_blank" id="terms">Terms & Conditions</a>
+              <a href="../pdfs/privacy_policy.pdf" download="privacy_policy.pdf" target="_blank" id="privacy">Privacy Policy</a>
+              <a href="../pdfs/faqs.pdf" download="faqs.pdf" target="_blank" id="faqs">FAQs</a>
+          </div>
+          <div class="socials">
+              <p>Socials</p>
+              <a href="https://www.facebook.com/" target="_blank" id="facebook">Facebook</a>
+              <a href="https://www.instagram.com/" target="_blank" id="instagram">Instagram</a>
+              <a href="https://www.tiktok.com/" target="_blank" id="tiktok">Tiktok</a>
+              <a href="https://www.youtube.com/" target="_blank" id="youtube">Youtube</a>
+          </div>
+      </div>
+  </div>    
+</footer>
+</body>
 </html>
